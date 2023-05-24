d707dacd364a43148679089c5dc50ec7755c6e52
Shiza Huda <shiza.huda421@gmail.com>
2023-05-24 20:33:28 +0500

    Create Android.py

---
+++ Android.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+apps_with_duplicates = pd.read_csv(r'C:\Users\shiza\Downloads\apps.csv')
+
+# Drop duplicates from apps_with_duplicates
+apps = apps_with_duplicates.drop_duplicates()
+
+# Print the total number of apps
+print('Total number of apps in the dataset = ', apps)
+
+# Have a look at a random sample of 5 rows
+print(apps.head())
+
+# List of characters to remove
+chars_to_remove = ['+',",",'$']
+# List of column names to clean
+cols_to_clean = ['Installs','Price']
+
+# Loop for each column in cols_to_clean
+for col in cols_to_clean:
+    # Loop for each char in chars_to_remove
+    for char in chars_to_remove:
+        # Replace the character with an empty string
+        apps[col] = apps[col].apply(lambda x: x.replace(char, ''))
+        
+# Print a summary of the apps dataframe
+print(apps.info())
+
+# Convert Installs to float data type
+apps['Installs'] = apps['Installs'].astype(float)
+
+# Convert Price to float data type
+apps['Price'] = apps['Price'].astype(float)
+
+# Checking dtypes of the apps dataframe
+print(apps.info())
+
+# Print the total number of unique categories
+num_categories = len(apps['Category'].unique())
+print('Number of categories = ', num_categories)
+
+# Count the number of apps in each 'Category'. 
+num_apps_in_category = apps['Category'].value_counts()
+
+# Sort num_apps_in_category in descending order based on the count of apps in each category
+sorted_num_apps_in_category = num_apps_in_category.sort_values(ascending=False)
+
+#Data Visualization
+plt.bar(num_apps_in_category.index,num_apps_in_category)
+plt.xticks(rotation=90)
+plt.show()
+
+# Average rating of apps
+avg_app_rating = apps['Rating'].mean()
+print('Average app rating = ', avg_app_rating)
+
+plt.hist(apps['Rating'],bins=20)
+plt.show()
+
+# Select rows where both 'Rating' and 'Size' values are present (ie. the two values are not null)
+apps_with_size_and_rating_present = apps[(~apps['Rating'].isnull()) & (~apps['Size'].isnull())]
+
+# Subset for categories with at least 250 apps
+large_categories = apps_with_size_and_rating_present.groupby(['Category']).filter(lambda x: len(x) >= 250)
+
+# Plot size vs. rating
+plt1 = sns.jointplot(x = large_categories['Size'], y = large_categories['Rating'])
+plt.show()
+
+# Select apps whose 'Type' is 'Paid'
+paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present['Type']=='Paid']
+
+# Plot price vs. rating
+plt2 = sns.jointplot(x = paid_apps['Price'], y = paid_apps['Rating'])
+plt.show()
+
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+# Select a few popular app categories
+popular_app_cats = apps[apps.Category.isin(['GAME', 'FAMILY', 'PHOTOGRAPHY',
+                                            'MEDICAL', 'TOOLS', 'FINANCE',
+                                            'LIFESTYLE','BUSINESS'])]
+
+# Examine the price trend by plotting Price vs Category
+ax = sns.stripplot(x = popular_app_cats['Price'], y = popular_app_cats['Category'], jitter=True, linewidth=1)
+ax.set_title('App pricing trend across categories')
+plt.show()
+
+# Select apps priced below $100
+apps_under_100 = popular_app_cats[popular_app_cats['Price']<100]
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 8)
+
+# Examine price vs category with the authentic apps (apps_under_100)
+ax = sns.stripplot(x ='Price' , y = 'Category', data = apps_under_100, jitter = True, linewidth = 1)
+ax.set_title('App pricing trend across categories after filtering for junk apps')
+plt.show()
+
+# Load user_reviews.csv
+reviews_df = pd.read_csv(r'C:\Users\shiza\Downloads\user_reviews.csv')
+
+# Join the two dataframes
+merged_df = apps.merge(reviews_df)
+
+# Drop NA values from Sentiment and Review columns
+merged_df = merged_df.dropna(subset = ['Sentiment', 'Review'])
+
+sns.set_style('ticks')
+fig, ax = plt.subplots()
+fig.set_size_inches(11, 8)
+
+# User review sentiment polarity for paid vs. free apps
+ax = sns.boxplot(x = 'Type', y = 'Sentiment_Polarity', data = merged_df)
+ax.set_title('Sentiment Polarity Distribution')
+plt.show()
